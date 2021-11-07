1eecd3476c1ea0845ac5483a6069cfc55d660496
hadasevers <93738177+hadasevers@users.noreply.github.com>
2021-11-07 12:14:10 +0200

    test

---
+++ main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "NumClass.h"
+
+#define FALSE 0
+#define TRUE 1
+
+int main(){
+
+    int min=0, max=0;
+    printf("enter a number \n");
+    scanf(" %d" , &min);
+    printf("enter a bigger number \n");
+    scanf(" %d" , &max);
+    int num=min;
+    if (min>max){
+        num=max;
+        max=min;
+        min=num;
+    }
+
+    printf("The Armstrong numbers are:");
+    while (num<=max)
+    { if (isArmstrong(num)==TRUE)
+           printf(" %d",num);
+      num++;
+    }
+
+    num=min;
+    printf("\n");
+
+    printf("The Palindromes are:");
+    while (num<=max)
+    { if (isPalindrome(num)==TRUE)
+           printf(" %d",num);
+      num++;
+    }
+
+    num=min;
+    printf("\n");
+
+    printf("The Prime numbers are:");
+    while (num<=max)
+    { if (isPrime(num)==TRUE)
+           printf(" %d",num);
+      num++;
+    }
+
+    num=min;
+    printf("\n");
+
+    printf("The Strong numbers are:");
+    while (num<=max)
+    { if (isStrong(num)==TRUE)
+           printf(" %d",num);
+      num++;
+    }
+    printf("\n");
+    return 0;
+}
